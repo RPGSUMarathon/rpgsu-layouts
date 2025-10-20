a70179a27a373801e7b2dfe057b22194d22d9871
mbcsalmeida <mbcsalmeida@gmail.com>
2025-10-20 08:27:21 +0200

    positioning blocks

--- src/browser/graphics/views/intermission.tsx
+++ src/browser/graphics/views/intermission.tsx
@@ -6,7 +6,14 @@ const Intermission = () => {
 
   return (
     <ThemeProvider>
-      <></>
+      <div className='w-full h-full flex flex-col'>
+        <div className='h-[260px] w-full bg-white'>
+
+        </div>
+        {/* Image */}
+        <div className='absolute top-[260px] left-0'></div>
+        <div className='absolute top-[260px] right-0 h-[760px] w-[720px] h-[760px] bg-red-100'></div>
+      </div>
     </ThemeProvider>
   );
 };
