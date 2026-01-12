bd945cb4e1127eb5c7d1e840fa92904f47491cba
Paweł Zatoka <github@hoxi.me>
2026-01-12 20:22:23 +0100

    fix(dependencies): fix dependency build scripts not running

--- install-submodules.js
+++ install-submodules.js
@@ -2,7 +2,7 @@ const {exec} = require('node:child_process')
 const path = require('node:path')
 
 console.log('Installing Speedcontrol dependencies...')
-exec('pnpm i -P', {
+exec('pnpm i -P --ignore-scripts', {
   cwd: path.join(__dirname, 'bundles/nodecg-speedcontrol')
 }, (error, stdout) => {
   console.log(stdout)
@@ -14,7 +14,7 @@ exec('pnpm i -P', {
 
 
 console.log('Installing foobar plugin dependencies...')
-exec('pnpm i', {
+exec('pnpm i --ignore-scripts', {
   cwd: path.join(__dirname, 'bundles/nodecg-foobar2000-controller')
 }, (error, stdout) => {
   console.log(stdout)
