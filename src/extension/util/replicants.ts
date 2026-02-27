d5263c61c55aba54e563cf9ac8cd416e74259c64
mbcsalmeida <mbcsalmeida@gmail.com>
2026-02-27 10:36:30 +0100

    removed obsolete code from affiliate event

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -6,14 +6,7 @@ const nodecg = get();
 
 export const currentOBSScene =
   nodecg.Replicant<CurrentOBSScene>("currentOBSScene");
-export const currentDay = nodecg.Replicant<string>(
-  "currentDayLogoAtIntermission",
-);
+
 export const commentators = nodecg.Replicant<Commentator[]>("commentators", {
   defaultValue: [],
 });
-
-//Layout silly utilities
-export const gameTextSize = nodecg.Replicant<string>("layoutGameTextSize");
-export const runnerTextSize = nodecg.Replicant<string>("runnerTextSize");
-export const categoryTextSize = nodecg.Replicant<string>("categoryTextSize");
