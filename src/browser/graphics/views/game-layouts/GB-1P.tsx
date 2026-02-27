8a5a6d0aa53bbfded12ea13470f4646129dc6bcd
mbcsalmeida <mbcsalmeida@gmail.com>
2026-02-27 10:36:57 +0100

    fixed GB just cause you never know

--- src/browser/graphics/views/game-layouts/GB-1P.tsx
+++ src/browser/graphics/views/game-layouts/GB-1P.tsx
@@ -1,101 +1,21 @@
-import useCameraOn from "../../../hooks/useCameraOn";
-import useCommentators from "../../../hooks/useCommentators";
-import useCurrentRun from "../../../hooks/useCurrentRun";
-import {
-  useCommentatorColumnSize,
-  useRunnerTextSize,
-} from "../../../hooks/useLayoutTextSize";
 import { Header } from "../../components/Header/Header";
-import { NoCamera } from "../../components/NoCamera";
-import { RunnerBox } from "../../components/RunTexts/RunnerBox";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 import { ThemeProvider } from "../../components/theme-provider";
-import Bluesky from "../../img/icons/bluesky.png";
-import Twitch from "../../img/icons/twitch.png";
-import Youtube from "../../img/icons/youtube.png";
-import Logo from "../../img/logo-text.png";
-import Waves from "../../img/waves.png";
+import Background from "../../img/online-background.png";
 
-type Props = {
-  className?: string;
-};
-
-export const SidebarSocialMedia = ({ className }: Props) => {
+const SidebarGB = () => {
   return (
-    <div
-      className={`text-center flex flex-row mx-auto my-10 items-center  ${className}`}
-    >
-      <img src={Logo} alt="Logo" className="w-[250px] ml-15 mr-15" />
-      <div className="grid grid-flow-row">
-        <div className="inline-flex items-center gap-5 text-4xl">
-          <img src={Twitch} alt="Twitch Logo" />
-          <span>@rpgsu</span>
-        </div>
-        <div className="inline-flex items-center gap-5 text-4xl">
-          <img src={Youtube} alt="Youtube Logo" />
-          <span>@rpgsumarathon</span>
-        </div>
-        <div className="inline-flex items-center gap-5 text-4xl">
-          <img src={Bluesky} alt="Bluesky Logo" />
-          <span>@rpgsu</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const SidebarGB = () => {
-  const currentRun = useCurrentRun();
-  const cameraOn = useCameraOn();
-  const commentators = useCommentators();
-  const commentatorsColumns = useCommentatorColumnSize();
-
-  const player = currentRun?.teams[0]?.players[0];
-
-  const runnerTextSize = useRunnerTextSize();
-
-  return (
-    <div className={`w-[832px] h-[980px] border-r-3 border-white"`}>
-      {cameraOn && cameraOn ? (
-        <div
-          id="CameraBox"
-          className="w-full h-[337.5px] border-b-3 border-white"
-        />
-      ) : (
-        <div className="h-[337.5px] border-b-3 border-white">
-          <NoCamera />
-        </div>
-      )}
+    <div className="flex h-[900px]">
+      <Sidebar />
       <div
-        className="bg-[#278178] h-full"
-        style={{
-          backgroundImage: `url(${Waves})`,
-        }}
-      >
-        <RunnerBox
-          twitch={player?.social.twitch}
-          youtube={player?.social.youtube}
-          textSize={runnerTextSize}
-          runner
-          pronouns={player?.pronouns}
-          name={player?.name ?? ""}
-        />
-        {commentators.length > 0 && (
-          <div
-            className={`grid ${commentatorsColumns > 0 ? `grid-cols-${commentatorsColumns}` : "grid-cols-2"} `}
-          >
-            {commentators.map((runner) => (
-              <RunnerBox
-                key={runner.name}
-                runner={false}
-                className="border-r-3 border-white"
-                pronouns={runner.pronouns}
-                name={runner.name}
-              />
-            ))}
-          </div>
-        )}
-        <SidebarSocialMedia className="" />
-      </div>
+        className="w-[580px] border-r-5 border-l-white"
+        style={{ backgroundImage: `url(${Background})` }}
+      />
+      <div className="flex-none h-full aspect-square " />
+      <div
+        className="w-[580px] border-l-5 border-l-white"
+        style={{ backgroundImage: `url(${Background})` }}
+      />
     </div>
   );
 };
