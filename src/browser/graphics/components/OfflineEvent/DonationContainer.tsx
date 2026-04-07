39ef634d6143e794c76b4d6905863d00875577b4
mbcsalmeida <mbcsalmeida@gmail.com>
2026-04-07 17:38:04 +0200

    test donation container

---
+++ src/browser/graphics/components/OfflineEvent/DonationContainer.tsx
@@ -0,0 +1,41 @@
+import { type DonationProps } from "@rpgsu-layouts/types/custom/intermission-elements";
+import { AutoTextSize } from "auto-text-size";
+
+export const DonationContainer = ({ name, amount, message }: DonationProps) => {
+  return (
+    <div className="w-full flex flex-col">
+      <div className="flex w-full">
+        <div className="flex-1 flex auto-text-size-override ridge-inner">
+          <AutoTextSize
+            mode="oneline"
+            minFontSizePx={10}
+            maxFontSizePx={28}
+            className="px-2"
+          >
+            {name}
+          </AutoTextSize>
+        </div>
+        <div className="flex-1 flex auto-text-size-override ridge-inner">
+          <AutoTextSize
+            mode="oneline"
+            minFontSizePx={12}
+            maxFontSizePx={28}
+            className="px-1"
+          >
+            {amount}
+          </AutoTextSize>
+        </div>
+      </div>
+      <div className="w-full ridge-inner">
+        <AutoTextSize
+          className="font-light px-1 "
+          mode="box"
+          minFontSizePx={18}
+          maxFontSizePx={34}
+        >
+          {message}
+        </AutoTextSize>
+      </div>
+    </div>
+  );
+};
--- src/browser/graphics/views/intermission.tsx
+++ src/browser/graphics/views/intermission.tsx
@@ -1,23 +1,28 @@
+import useCommentators from "../../../browser/hooks/useCommentators";
 import useCurrentRun from "../../hooks/useCurrentRun";
 import useUpcomingRuns from "../../hooks/useUpcomingRuns";
 import { render } from "../../render";
 import { RunContainer } from "../components/OfflineEvent/CurrentRunContainer";
+import { DonationContainer } from "../components/OfflineEvent/DonationContainer";
 import { MusicPlayerContainer } from "../components/OfflineEvent/MusicPlayerContainer";
 import { UpcomingRunContainer } from "../components/OfflineEvent/UpcomingRunContainer";
 import { ThemeProvider } from "../components/theme-provider";
 
 const Intermission = () => {
   const currentRun = useCurrentRun();
-
+  const commentators = useCommentators();
   const upcomingRuns = useUpcomingRuns(2, currentRun?.id ?? "");
+
+  const exampleDonation =
+    "Thank you guys so much for putting together an RPG event, it's really cool and and Im very grateful to be part of it and Im expressing it through this very long message.";
 
   return (
     <ThemeProvider className="" style={{ backgroundImage: `` }}>
       <div className="flex flex-row">
         <div className="w-[395px] h-[520px] box2" />
         <div className="w-[1490px] h-[520px] relative">
-            <div className="absolute w-full h-[30px] box2" />
-            <div className="absolute w-full h-[30px] box2 bottom-0" />
+          <div className="absolute w-full h-[30px] box2" />
+          <div className="absolute w-full h-[30px] box2 bottom-0" />
         </div>
         <div className="w-[395px] h-[520px]  box2-inverted" />
       </div>
@@ -41,6 +46,18 @@ const Intermission = () => {
         <div className="h-full w-[528px] bg-[#6e7c9c] box2">
           <div className="w-full ridge-inner text-center">
             <h2 className="text-5xl p-1">Donations</h2>
+            {commentators.length > 0 && (
+              <div className="w-full">
+                {commentators.map((runner) => (
+                  <DonationContainer
+                    key={runner.id}
+                    name={runner.name}
+                    amount="9999"
+                    message={exampleDonation}
+                  />
+                ))}
+              </div>
+            )}
           </div>
         </div>
         <div className="h-full w-[500px] bg-green-50 box2">
