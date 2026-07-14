1cb516e83856c361dc356d90076ad6dd595cbf81
mbcsalmeida <mbcsalmeida@gmail.com>
2026-07-14 13:31:21 +0200

    preset for multiple info during intermission screen

--- src/browser/graphics/components/OfflineEvent/IntermissionInfoContainer.tsx
+++ src/browser/graphics/components/OfflineEvent/IntermissionInfoContainer.tsx
@@ -1,9 +1,24 @@
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
 import ILGALogo from "../../img/offline2026/ilga-full-logo.png";
 
 export const IntermissionInfoContainer = () => {
+  const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
+
   return (
     <div className="ridge-inner w-full h-[290px] bg-(--color-offline-info) place-content-center p-5">
-      <img src={ILGALogo} />
+      <AnimatePresence mode="wait">
+        <motion.span
+          key={currentComponentIndex}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-100%" }}
+          transition={{ duration: 0.5 }}
+          className="block w-full"
+        >
+          <img src={ILGALogo} />
+        </motion.span>
+      </AnimatePresence>
     </div>
   );
 };
