00fafa0d29a15588203e6a4ececc0bf9354cd569
mbcsalmeida <mbcsalmeida@gmail.com>
2026-08-07 23:30:38 +0200

    test component to test notification

--- src/browser/graphics/components/OfflineEvent/IntermissionInfo/Location.tsx
+++ src/browser/graphics/components/OfflineEvent/IntermissionInfo/Location.tsx
@@ -3,7 +3,7 @@ import Zutphen from "../../../img/zutphen.png";
 
 export const LocationContainer = () => {
   return (
-    <div className="flex flex-col w-full h-full text-2xl text-black items-center">
+    <div className="flex flex-col w-full h-full gap-5 text-2xl text-black items-center">
       <span className="text-4xl">We are currently live from</span>
       <img width={450} src={Zutphen} />
       <span className="text-3xl">Fletcher Hotel-Resort Zutphen</span>
