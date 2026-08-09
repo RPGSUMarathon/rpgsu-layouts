e265cc53c5599fc27e1fbb76895c1218e7c4e5a0
mbcsalmeida <mbcsalmeida@gmail.com>
2026-08-09 21:34:38 +0200

    update countdown screen

--- src/browser/graphics/views/countdown.tsx
+++ src/browser/graphics/views/countdown.tsx
@@ -2,8 +2,8 @@ import { useReplicant } from "@nodecg/react-hooks";
 import { type Countdown } from "@rpgsu-layouts/types";
 import { render } from "../../render";
 import { ThemeProvider } from "../components/theme-provider";
+import Logo from "../img/offline2026/offline26_logo.png";
 import Background from "../img/offline2026/offline-background.png";
-import Logo from "../img/online2026/base-online-logo.png";
 
 export const CountdownView = () => {
   const [countdown] = useReplicant<Countdown>("countdown");
@@ -12,11 +12,11 @@ export const CountdownView = () => {
     <ThemeProvider theme="offline" style={{ background: `url(${Background})` }}>
       <div className="w-full flex flex-col text-center align-center gap-10">
         <div className="w-full text-center flex flex-row align-center justify-center">
-          <img className="w-1/3" src={Logo} />
+          <img className="w-1/2" src={Logo} />
         </div>
         <div>
-          <div className="text-5xl font-semibold">Stream starting in: </div>
-          <div className="text-9xl font-bold">{countdown?.formatted}</div>
+          <div className="text-5xl font-semibold text-shadow-md text-shadow-black">Stream starting in: </div>
+          <div className="text-9xl font-bold text-shadow-md text-shadow-black">{countdown?.formatted}</div>
         </div>
       </div>
     </ThemeProvider>
