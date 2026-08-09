a468daa7f0687686c89e2e5e99de39026f307e4f
mbcsalmeida <mbcsalmeida@gmail.com>
2026-08-10 01:06:13 +0200

    rerun component

--- src/browser/graphics/views/rerun.tsx
+++ src/browser/graphics/views/rerun.tsx
@@ -1,36 +1,14 @@
-import type { RunData } from "speedcontrol/src/types";
-import type { RunDataActiveRunSurrounding } from "speedcontrol/src/types/schemas";
 import { render } from "../../render";
-import { timeToRun as timeToRunFunc } from "../../time-to-run";
 import { ThemeProvider } from "../components/theme-provider";
 import Logo from "../img/offline2026/offline26_logo.png";
 
-const runDataArray = nodecg.Replicant<RunData[]>(
-  "runDataArray",
-  "nodecg-speedcontrol",
-);
-const runDataActiveRunSurrounding =
-  nodecg.Replicant<RunDataActiveRunSurrounding>(
-    "runDataActiveRunSurrounding",
-    "nodecg-speedcontrol",
-  );
-
-function getNextRun() {
-  return (runDataArray.value ?? []).find(
-    (run) => run.id === runDataActiveRunSurrounding.value?.next,
-  );
-}
-
 export const Rerun = () => {
-  const nextRun = getNextRun();
-  const timeToRun = timeToRunFunc(nextRun);
-
   return (
     <ThemeProvider theme="offline" className="w-full h-full">
-      <div className="bottom-[160px] left-[40px] absolute bg-(--color-world-bg) box2-inverted p-5 text-center rotate-15 ">
-        <h1 className="text-2xl engraved">This is a rerun. </h1>
-        <h1 className="text-2xl engraved">
-          Live runs will be back in {timeToRun}
+      <div className="rerun-div bg-(--color-offline-main) text-center rounded-xl shadow shadow-md shadow-black w-[1160px] h-[50px]">
+        <h1 className="text-2xl engraved font-semibold">
+          This is a rerun. Live runs at RPGSU Offline 2026 will be back in the
+          morning.
         </h1>
       </div>
     </ThemeProvider>
